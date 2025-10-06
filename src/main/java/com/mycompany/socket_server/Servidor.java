129579092c3312cd5a10640a1b3ecd41c43e8fb0
Luiz Gustavo <pereira.since2004@gmail.com>
2025-10-06 20:11:15 -0300

    sending_objects_socket_java_commit

---
+++ src/main/java/com/mycompany/socket_server/Servidor.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.socket_server;
+
+import java.util.Scanner;
+import java.io.*;
+import java.net.*;
+
+public class Servidor extends Thread {
+
+    private Socket con;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    public Servidor(Socket socket) {
+        try {
+            con = socket;
+            in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            out = new PrintWriter(con.getOutputStream(), true);
+        } catch (IOException ioe) {
+            System.err.println("Problemas de IO");
+        }
+    }
+
+    public void run() {
+        try {
+            out.println("Bem vindo!"); //Escreve ao cliente
+            String mensagem = "";
+            while ((mensagem = in.readLine()) != null) {
+                System.out.println(mensagem);
+            }
+        } catch (IOException ioe) {
+            System.err.println("Problemas de IO");
+        }
+    }
+}
